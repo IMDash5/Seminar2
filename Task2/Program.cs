ea3342228fcc7c8cac389d489ee421ba5d9f1608
Ivan <mmavrin80@gmail.com>
2022-03-19 19:44:03 +0300

    task2

---
+++ Task2/Program.cs
@@ -0,0 +1,7 @@
+﻿Random rnd = new Random();
+int value = rnd.Next(10, 99);
+Console.WriteLine(value);
+
+ int maxNum = value / 10;
+ if(maxNum < value % 10) maxNum = value % 10;
+ Console.Write(maxNum);
