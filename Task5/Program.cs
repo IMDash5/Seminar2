d1e1dabba6e54edd9ed5c379a9d85088891f89f5
Ivan <mmavrin80@gmail.com>
2022-03-19 21:16:16 +0300

    task5

---
+++ Task5/Program.cs
@@ -0,0 +1,29 @@
+﻿int a = Num("Введите n ");
+
+int Num(string arg)
+{
+    Console.Write(arg);
+    string n = Console.ReadLine();
+    int N = int.Parse(n);
+    return N;
+}
+
+
+
+
+
+int Module(int N)
+{
+    int mod = N;
+    if (N < 0)
+    {
+        return -mod;
+    }
+    else
+    {
+        return mod;
+    }
+    return mod;
+}
+
+Console.WriteLine(Module(a));
