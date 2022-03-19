6145560e633889a2e7fd769a998e61acc1d0cc29
Ivan <mmavrin80@gmail.com>
2022-03-19 20:23:31 +0300

    task4

---
+++ Task 4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+string num = Console.ReadLine();
+int Num = int.Parse(num);
+
+if (Num % 7 == 0)
+{
+    if (Num % 23 == 0) 
+    {
+        Console.WriteLine("Da");
+    }
+}
+else 
+{
+    Console.WriteLine("Net");
+}
